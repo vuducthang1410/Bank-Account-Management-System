1397ab0b01880c6b93951ac87501f66700e3b792
Vũ Đức Thắng <123xxthang@gmail.com>
2025-01-14 14:50:34 +0700

    create project

---
+++ CustomerSerivce/src/test/java/org/demo/customerserivce/CustomerSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.customerserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
