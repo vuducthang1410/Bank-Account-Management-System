4aa633d71647d453b3e21b85c08d7d924c81a448
Vũ Đức Thắng <123xxthang@gmail.com>
2025-01-14 15:45:39 +0700

    add other service

---
+++ ApiGetwayService/src/main/java/org/demo/apigetwayservice/ApiGetwayServiceApplication.java
@@ -0,0 +1,13 @@
+package org.demo.apigetwayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetwayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetwayServiceApplication.class, args);
+    }
+
+}
