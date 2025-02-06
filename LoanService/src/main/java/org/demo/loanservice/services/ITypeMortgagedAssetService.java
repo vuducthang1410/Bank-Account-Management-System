9606e15ea39efa18afda72f76085336d1394b9f2
Vũ Đức Thắng <123xxthang@gmail.com>
2025-02-06 17:11:41 +0700

    collapse project

--- LoanService/src/main/java/org/demo/loanservice/services/ITypeMortgagedAssetService.java
+++
@@ -1,7 +0,0 @@
-package org.demo.loanservice.services;
-
-import org.demo.loanservice.common.DataResponseWrapper;
-import org.demo.loanservice.dto.request.TypeMortgagedAssetsRq;
-
-public interface ITypeMortgagedAssetService extends IBaseService<TypeMortgagedAssetsRq>{
-}
